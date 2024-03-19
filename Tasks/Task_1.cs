b0a7d19e89718edb4c70722a81ebd0f91f7f1c05
Threemasks12 <jongeorgmagnussen@gmail.com>
2024-03-19 14:39:21 +0100

    compleated task 1, starting task 2

--- Tasks/Task_1.cs
+++ Tasks/Task_1.cs
@@ -1,3 +1,4 @@
+
 /* 
 ## Task : Functions are a popping
 Create the following functions: 
@@ -16,11 +17,13 @@ double Square(double value)
 square = Square(squareNumber);
 
 Print($"the square of {squareNumber} is {square}");
+
 Print("-------------------------------------------------------------------------------------------");
-Print("");
 //-------------------------------------------------------------------------------------------
 
+
 // A function that returns a length in mm assuming it has been given a length in inches.
+
 
 double inches = 5;
 double mm = Converter(inches);
@@ -32,26 +35,71 @@ static double Converter(double inches)
 };
 
 Print($"{inches} is equal to {mm} millimeters");
+
 Print("-------------------------------------------------------------------------------------------");
-Print("");
+
 //------------------------------------------------------------------------------------------
+
 
 // A function that returns the root of a number
 
+double valueNumber = 25;
+
+double theSquareRoot(double baseNumber)
+{
+    double exponet;
+    return Math.Sqrt(baseNumber);
+}
+
+double exponet = theSquareRoot(valueNumber);
+
+Print($"the square root of 25 is {exponet}");
+
+Print("-------------------------------------------------------------------------------------------");
+//------------------------------------------------------------------------------------------
 
 
+// A function that returns the cube of a number
+int cubeNumber = 9;
+double numberInCube;
 
+double Cube(double baseValue)
+{
+    return baseValue * baseValue * baseValue;
+}
 
+numberInCube = Cube(cubeNumber);
 
+Print($"The cube of {cubeNumber} is {numberInCube}");
 
+Print("-------------------------------------------------------------------------------------------");
+//------------------------------------------------------------------------------------------
 
-
-
-// A function that returns the cube of a number 
 // A function that returns the area of a circle given the radius. 
+
+double pi = 3.14;
+double radius = 8;
+
+double calculateAreaOfCircile(double PI, double r)
+{
+    return PI * r * r;
+}
+
+double area = calculateAreaOfCircile(pi, radius);
+Print($"The are of the circile is {area}");
+
+Print("-------------------------------------------------------------------------------------------");
+
+//------------------------------------------------------------------------------------------
+
 // A function that returns a greeting, given a name.
 
+void Greeting()
+{
+    Print("Good day Christian and Tony (•_•) ( •_•)>⌐■-■ (⌐■_■)");
+}
 
+Greeting();
 
 
 
